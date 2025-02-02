4e2112ab9d52730b1a5b87d5965c6e30f0a37b88
Eliyanan <84735278+adams1864@users.noreply.github.com>
2025-02-02 10:31:40 -0800

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,8 @@
+-- Query the column details of the Books table in alx_book_store
 USE alx_book_store;
 
-SHOW CREATE TABLE books;
+-- Retrieve column names and types for the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
